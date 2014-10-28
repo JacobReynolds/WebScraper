9b24a1faf00f7e0b22c283ba8c8499b58ba6ea70
Jacob Reynolds <Jacob.LReynolds@Yahoo.com>
2014-10-27 22:33:13 -0500

    Main functions work, but not together
    I can now access Moodle and get my grades returned, and send an e-mail
    to myself.  Code is not very pretty or efficient, next step is to fix
    that, and then find out how to save data to a text file on the computer
    for comparison when it checks for new grades.

--- src/webreader/WebReader.java
+++ src/webreader/WebReader.java
@@ -6,12 +6,21 @@
 
 package webreader;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.DefaultCssErrorHandler;
+import com.gargoylesoftware.htmlunit.IncorrectnessListener;
+import com.gargoylesoftware.htmlunit.Page;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import com.gargoylesoftware.htmlunit.WebClient; 
+import com.gargoylesoftware.htmlunit.WebWindow;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlButtonInput;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
@@ -19,6 +28,8 @@ import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -27,6 +38,9 @@ import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.Transport;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.css.sac.CSSException;
+import org.w3c.css.sac.CSSParseException;
 
 /**
  *
@@ -37,8 +51,8 @@ public class WebReader {
 
 
     public static void main(String[] args) throws Exception {
-        String message = "This is a test e-mail";
-        sendEmail(message);
+//        String message = "Yay Jake can copy and paste from the internet!";
+//        sendEmail(message);
        
 //        String url = "http://moodle.umn.edu/";
 //        Document doc = Jsoup.connect(url).get();
@@ -54,41 +68,64 @@ public class WebReader {
         
         //Could be how to submit username and password
 //        HtmlPage page3;
-//   page3 = webClient.getPage("Website");
-//   HtmlForm loginForm = page3.getFormByName("loginForm");
-//   HtmlTextInput username = loginForm.getInputByName("NameofUsernameElement");
-//   HtmlPasswordInput pass = loginForm.getInputByName("NameofPassowordElement");
-//   HtmlSubmitInput b = loginForm.getInputByValue("LoginButtonValue");
-//
-//   username.setValueAttribute("Actualy Username");
-//   pass.setValueAttribute("Actual Password");
-//   HtmlPage page2;
-//   page2 = b.click();
-        
 //        final WebClient webClient = new WebClient(BrowserVersion.CHROME);
-////
-////    // Get the first page
-//    final HtmlPage page1 = webClient.getPage("http://www.google.com");
-//    
-//    // Get the form that we are dealing with and within that form, 
-//    // find the submit button and the field that we want to change.
-//    final HtmlForm form = page1.getFormByName("search");
+//        webClient.getOptions().setUseInsecureSSL(true);
+//        page3 = webClient.getPage("Website");
+//        HtmlForm loginForm = page3.getFormByName("loginForm");
+//        HtmlTextInput username = loginForm.getInputByName("NameofUsernameElement");
+//        HtmlPasswordInput pass = loginForm.getInputByName("NameofPassowordElement");
+//        HtmlSubmitInput b = loginForm.getInputByValue("LoginButtonValue");
 //
-//    final HtmlSubmitInput button = form.getInputByName("Sign In");
-//    final HtmlTextInput userField = form.getInputByName("j_username");
-//    final HtmlTextInput passwordField = form.getInputByName("j_password");
-//
-//    // Change the value of the text field
-//    userField.setValueAttribute("reyno511");
-//    passwordField.setValueAttribute("testing");
-//
-//    // Now submit the form by clicking the button and get back the second page.
-////    final HtmlPage page2 = button.click();
-//    button.click();
+//        username.setValueAttribute("Actual Username");
+//        pass.setValueAttribute("Actual Password");
+//        HtmlPage page2;
+//        page2 = b.click();
+        
+        System.out.println("program is running");
+        //---Suppresses unneeded warnings for CSS and other things
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+    
+        String password = "testing";//---Insert password here
+        final WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setUseInsecureSSL(true);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
 
-//    webClient.closeAllWindows();
+//    // Get the first page
+    final HtmlPage page1 = webClient.getPage("https://moodle.umn.edu");
+//    System.out.println(page1.asText());
+    // Get the form that we are dealing with and within that form, 
+    // find the submit button and the field that we want to change.
+//    final HtmlForm form = page1.getFirstByXPath("//form[@action='/idp/umn/authn']");
+    final HtmlForm form = page1.getElementByName("lform");
+
+    final HtmlSubmitInput button = form.getInputByValue("Sign In");
+    final HtmlTextInput userField = form.getInputByName("j_username");
+    final HtmlPasswordInput passwordField = form.getInputByName("j_password");
+    
+    // Change the value of the text field
+    userField.setValueAttribute("reyno511");
+    passwordField.setValueAttribute(password);
+    WebWindow window = page1.getEnclosingWindow();
+    // Now submit the form by clicking the button and get back the second page.
+//    final HtmlPage page2 = button.click();
+        button.click();
+
         
+        final HtmlPage page2 = webClient.getPage("https://ay14.moodle.umn.edu/my/");
+
+
+//        DomElement element = page2.getElementById("yui_3_13_0_3_1414454332948_11");
+//            DomElement element = page2.getFirstByXPath("//span[@class='content']");
+//                System.out.println(page2.asText());
+//            System.out.println(element);
+//        String text = element.getTextContent();
+//        System.out.println(text);
+//String text = element.getTextContent();
+        String grades = clean(page2.asText());
+        System.out.println(grades.toString());
+            
         
+    webClient.closeAllWindows();
         
          
         }
@@ -132,6 +169,54 @@ public class WebReader {
         //---Used code from https://www.youtube.com/watch?v=sHC8YgW21ho
         //---for the above method
     }
+    
+   private static String clean(String dirtyString)
+   {
+       String cleanString = "";
+       List wordList = new LinkedList();
+       String newWord = "";
+       int beginningOfWord = 0;
+       //---Some words are seperated by /br which is not a space
+       //---but is always followed by an uppercase letter
+       String newLine = System.getProperty("line.separator");
+       
+       for (int i = 0; i < dirtyString.length(); i++) {
+           String convertCharToString = "";
+           convertCharToString += dirtyString.charAt(i);
+           
+           if (dirtyString.charAt(i) == ' ' || convertCharToString.equals(newLine))
+           {
+               for (int j = beginningOfWord; j < i; j++) {
+                   newWord += dirtyString.charAt(j);
+               }
+               beginningOfWord = i+1;
+               wordList.add(newWord);
+           }
+           
+               newWord = "";
+       }
+       
+       for (int i = 0; i < wordList.size(); i++) {
+
+           if (wordList.get(i).equals("My") && wordList.get(i + 1).equals("Grades")
+                   && !wordList.get(i - 1).equals("Skip"))
+           {
+               
+               for (int j = i + 2; j < wordList.size(); j++) {
+                   if (wordList.get(j).equals("Skip"))
+                   {   
+                       return cleanString;
+                   }
+                   else
+                   {
+                       cleanString += wordList.get(j) + System.lineSeparator();
+                   }
+               }
+           }
+           
+       }
+       return cleanString;
+   }
     }
 
 
