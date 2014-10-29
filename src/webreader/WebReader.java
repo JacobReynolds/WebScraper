631dc6d42c11704f8b2b2ad73aeb0319b497430f
Jacob Reynolds <Jacob.LReynolds@Yahoo.com>
2014-10-28 20:24:56 -0500

    The program is fully working
    When ran the program will login, compare grades to the grades from the
    last time that it checked, and e-mail you if they have changed.  Need
    to take away some hardcoding next.

--- src/webreader/WebReader.java
+++ src/webreader/WebReader.java
@@ -6,25 +6,23 @@
 
 package webreader;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.DefaultCssErrorHandler;
-import com.gargoylesoftware.htmlunit.IncorrectnessListener;
-import com.gargoylesoftware.htmlunit.Page;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import com.gargoylesoftware.htmlunit.WebClient; 
-import com.gargoylesoftware.htmlunit.WebWindow;
-import com.gargoylesoftware.htmlunit.html.DomElement;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
-import com.gargoylesoftware.htmlunit.html.HtmlButtonInput;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -39,93 +37,66 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.Transport;
 import org.apache.commons.logging.LogFactory;
-import org.w3c.css.sac.CSSException;
-import org.w3c.css.sac.CSSParseException;
 
-/**
- *
- * @author colleen-reynolds
- */
+
 public class WebReader {
 
 
 
     public static void main(String[] args) throws Exception {
-//        String message = "Yay Jake can copy and paste from the internet!";
-//        sendEmail(message);
-       
-//        String url = "http://moodle.umn.edu/";
-//        Document doc = Jsoup.connect(url).get();
-//
-//        String test = doc.title();
-//        System.out.println(test);
-//        
-//        Elements txtArea = doc.select("#quickpost");
-//        txtArea.text();
+  
         
-        
-        
-        
-        //Could be how to submit username and password
-//        HtmlPage page3;
-//        final WebClient webClient = new WebClient(BrowserVersion.CHROME);
-//        webClient.getOptions().setUseInsecureSSL(true);
-//        page3 = webClient.getPage("Website");
-//        HtmlForm loginForm = page3.getFormByName("loginForm");
-//        HtmlTextInput username = loginForm.getInputByName("NameofUsernameElement");
-//        HtmlPasswordInput pass = loginForm.getInputByName("NameofPassowordElement");
-//        HtmlSubmitInput b = loginForm.getInputByValue("LoginButtonValue");
-//
-//        username.setValueAttribute("Actual Username");
-//        pass.setValueAttribute("Actual Password");
-//        HtmlPage page2;
-//        page2 = b.click();
         
         System.out.println("program is running");
+       
         //---Suppresses unneeded warnings for CSS and other things
         LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
     
-        String password = "testing";//---Insert password here
+        //---Creating webClient and allowing access to website
+        //---Also supresses some unneeded script warnings
         final WebClient webClient = new WebClient(BrowserVersion.CHROME);
         webClient.getOptions().setUseInsecureSSL(true);
         webClient.getOptions().setThrowExceptionOnScriptError(false);
+        
+        //---Get the first page
+        final HtmlPage page1 = webClient.getPage("https://moodle.umn.edu");
 
-//    // Get the first page
-    final HtmlPage page1 = webClient.getPage("https://moodle.umn.edu");
-//    System.out.println(page1.asText());
-    // Get the form that we are dealing with and within that form, 
-    // find the submit button and the field that we want to change.
-//    final HtmlForm form = page1.getFirstByXPath("//form[@action='/idp/umn/authn']");
-    final HtmlForm form = page1.getElementByName("lform");
+        //---Gets the login form and username and password fields
+        final HtmlForm form = page1.getElementByName("lform");
 
-    final HtmlSubmitInput button = form.getInputByValue("Sign In");
-    final HtmlTextInput userField = form.getInputByName("j_username");
-    final HtmlPasswordInput passwordField = form.getInputByName("j_password");
+        final HtmlSubmitInput button = form.getInputByValue("Sign In");
+        final HtmlTextInput userField = form.getInputByName("j_username");
+        final HtmlPasswordInput passwordField = form.getInputByName("j_password");
     
-    // Change the value of the text field
-    userField.setValueAttribute("reyno511");
-    passwordField.setValueAttribute(password);
-    WebWindow window = page1.getEnclosingWindow();
-    // Now submit the form by clicking the button and get back the second page.
-//    final HtmlPage page2 = button.click();
+        //---Change the value of the text fields
+        userField.setValueAttribute("reyno511");//---Insert username here
+        passwordField.setValueAttribute("password");//---Insert password here
+       
+        //---Submits the form by clicking the button
         button.click();
 
-        
+        //---New page after clicking the button
         final HtmlPage page2 = webClient.getPage("https://ay14.moodle.umn.edu/my/");
 
-
-//        DomElement element = page2.getElementById("yui_3_13_0_3_1414454332948_11");
-//            DomElement element = page2.getFirstByXPath("//span[@class='content']");
-//                System.out.println(page2.asText());
-//            System.out.println(element);
-//        String text = element.getTextContent();
-//        System.out.println(text);
-//String text = element.getTextContent();
+        //---Sends the new page to be cleaned and parsed for grades
+        //---clean(String string) returns a string
         String grades = clean(page2.asText());
-        System.out.println(grades.toString());
-            
+
         
-    webClient.closeAllWindows();
+        if (!grades.equals(fileReader()))
+        {  
+        //---Updates the grades in the saved file it compares them to    
+        updateGrades(grades);
+        
+        //---Takes the new grades and sends them to the e-mail in the
+        //---sendEmail function
+        sendEmail(grades);
+        }
+        
+        //---Closes windows for current session
+        webClient.closeAllWindows();
+        
+        
         
          
         }
@@ -134,14 +105,16 @@ public class WebReader {
     {
         
         final String username = "xiixsnb@gmail.com";
-        final String password = "testing";//---Put in password here
+        final String password = "password";//---Put in password here
         
+        //---Sets server for gmail
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
         
+        //---Creates a new session
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
             return new PasswordAuthentication(username, password);
@@ -149,12 +122,13 @@ public class WebReader {
             }
         
         });
-        
+        //---Try catch loop for sending an email
+        //---Sends error email to the sender if catch is engaged.
         try {
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("xiixsnb@gmail.com"));
             message.setRecipient(Message.RecipientType.TO, new InternetAddress("reyno511@umn.edu"));
-            message.setSubject("This is a new email");
+            message.setSubject("Your grades have been updated");
             message.setText(inputMessage);
             Transport.send(message);
             DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
@@ -176,10 +150,13 @@ public class WebReader {
        List wordList = new LinkedList();
        String newWord = "";
        int beginningOfWord = 0;
-       //---Some words are seperated by /br which is not a space
-       //---but is always followed by an uppercase letter
+       //---Some words are seperated by a line seperator which is not a space
        String newLine = System.getProperty("line.separator");
        
+       //---Gets the string, breaks it into a list for every word,
+       //---then strips out only the grades.
+       //Is currently hardcoded for my layout for moodle, will soon find a way
+       //to make it more compatable with any type of moodle layout.
        for (int i = 0; i < dirtyString.length(); i++) {
            String convertCharToString = "";
            convertCharToString += dirtyString.charAt(i);
@@ -196,6 +173,7 @@ public class WebReader {
                newWord = "";
        }
        
+       //---Finds grades in the wordList
        for (int i = 0; i < wordList.size(); i++) {
 
            if (wordList.get(i).equals("My") && wordList.get(i + 1).equals("Grades")
@@ -217,6 +195,53 @@ public class WebReader {
        }
        return cleanString;
    }
+   
+   public static String fileReader() {
+       String answer = "";
+       
+       BufferedReader br = null;
+ 
+		try {
+ 
+			String sCurrentLine;
+ 
+			br = new BufferedReader(new FileReader("/Users/colleen-reynolds/Desktop/School/CSCI/Projects/WebScraper/Grades.txt"));
+ 
+			while ((sCurrentLine = br.readLine()) != null) {
+                                answer += sCurrentLine + System.lineSeparator();
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+ 
+	
+       
+       
+       
+       return answer;
+  }
+   
+   public static void updateGrades(String grades) throws FileNotFoundException, IOException
+   {
+       System.out.println("updating grades");
+       BufferedReader br = null;
+       BufferedWriter bw = null;
+       
+       br = new BufferedReader(new FileReader("/Users/colleen-reynolds/Desktop/School/CSCI/Projects/WebScraper/Grades.txt"));
+       bw = new BufferedWriter(new FileWriter("/Users/colleen-reynolds/Desktop/School/CSCI/Projects/WebScraper/Grades.txt", false));
+       
+       bw.write(grades);
+       bw.close();
+       br.close();
+   }
+   
     }
 
 
